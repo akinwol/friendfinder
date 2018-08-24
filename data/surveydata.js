a38410ff84feb33c43a4e3b1db598f79f62cc0c3
Nyra Dev <37726406+akinwol@users.noreply.github.com>
2018-08-24 03:43:02 -0400

    working version

---
+++ data/surveydata.js
@@ -0,0 +1,87 @@
+
+var surveyData = [
+    {
+        name:"Test Name 1",
+        photoLink:"https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350",
+        scores: [1,1,1,1,1,1]
+    },
+    {
+        name:"Test Name 2",
+        photoLink:"https://goo.gl/UCrwt5",
+        scores: [2,2,2,2,2,2]
+    },
+    {
+        name:"Test Name 3",
+        photoLink: "https://goo.gl/VCC4dJ",
+        scores: [3,3,3,3,3,3]
+    },
+    {
+        name:"Test Name 4",
+        photoLink: "https://goo.gl/uCk8zk",
+        scores: [4,4,4,4,4,4]
+    }
+  
+];
+
+var match = []
+
+var newPost =   {
+    name:"Test Name 3",
+    scores: [2,2,7,10]
+}
+
+
+// module.exports = surveyData
+module.exports = {
+    data:surveyData,
+    pick: match}
+
+
+var test1 = [3,5,6,9];
+var test2 = [4,1,8,11];
+var diff =[]
+var allDiff = []
+
+// for (let i = 0; i < test1.length; i++) {
+// // math.abs returns the absolute value of a number 
+// diff.push(Math.abs(test1[i]-test2[i]));
+    
+// };
+// const reducer = (accumulator, currentValue) => accumulator + currentValue;
+// var totalDiff = diff.reduce(reducer)
+// console.log(diff)
+// console.log(totalDiff)
+
+function compare(current, arr){
+// initialize a variable to store the comparison between the submitted and the current index
+    var diff = [];
+// loop through the length of the new submission 
+    for (let i = 0; i < current.length; i++) {
+        // math.abs returns the absolute value of a number 
+        // push the absolute difference to the diff array 
+        diff.push(Math.abs(current[i]-arr[i]));     
+        };
+// add all the values of the difference array and store in a variable
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        var totalDiff = diff.reduce(reducer)
+        // console.log(diff)
+        // console.log(totalDiff)
+// push the total amount of the difference into an all difference array.
+// all diff array will store the totals of each existing score 
+        allDiff.push(totalDiff)
+};
+
+// loop through existing friends survey data
+surveyData.forEach(function(element){
+    // console.log(element.score)
+// run the function for each index in the survey data 
+// function will store all the total diff into the all diff array 
+    compare(newPost.scores, element.scores)
+});
+
+console.log(allDiff)
+
+// this will give you the index of the lowest number
+var indexOfMin = allDiff.indexOf(Math.min.apply(Math, allDiff))
+console.log(indexOfMin)
+console.log("Name of match: ", surveyData[indexOfMin].name)
---
+++ routing/apiRoutes.js
@@ -0,0 +1,40 @@
+
+var surveyData = require("../data/surveydata");
+
+module.exports = function (app) {
+    app.get("/api/friends", function (req, res) {
+        res.json(surveyData.data);
+    });
+    app.post("/api/friends", function (req, res) {
+        // see survey data for explanation
+        var allDiff = []
+        function compare(current, arr) {
+            var diff = [];
+            for (let i = 0; i < current.length; i++) {
+                diff.push(Math.abs(current[i] - arr[i]));
+            };
+            const reducer = (accumulator, currentValue) => accumulator + currentValue;
+            var totalDiff = diff.reduce(reducer)
+            allDiff.push(totalDiff)
+        };
+
+        surveyData.data.forEach(function (element) {
+            compare(req.body.scores, element.scores)
+        });
+
+        // console.log(allDiff)
+
+        // this will give you the index of the lowest number
+        var indexOfMin = allDiff.indexOf(Math.min.apply(Math, allDiff))
+        // console.log(indexOfMin)
+        console.log("NAME OF MATCH: ", surveyData.data[indexOfMin].name)
+        surveyData.pick.push(surveyData.data[indexOfMin].name, surveyData.data[indexOfMin].photoLink);
+        surveyData.data.push(req.body);
+    });
+    app.get("/api/match", function(req, res){
+        res.send(surveyData.pick)
+    });
+};
+
+
+console.log(surveyData.data)
